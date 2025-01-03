e1178e573cb6c3f9ad56c2a09fd38fa2385d3d3d
daniele <daniele.fallaci@gmail.com>
2025-01-03 21:38:59 +0100

    wallpaper added and also yazi

--- hosts/danixOS/home/home-modules/swww/default.nix
+++ hosts/danixOS/home/home-modules/swww/default.nix
@@ -4,6 +4,7 @@
     pkgs.swww
   ];
 
+  # thanks: https://github.com/NixOS/nixos-artwork/tree/master/wallpapers
   home.file."wallpapers/wall1.png".source=./wallpapers/wall1.png;
   home.file."wallpapers/wall2.png".source=./wallpapers/wall2.png;
 
