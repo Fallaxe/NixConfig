23cae4dafc55d04535e2892909ef2f9dcade7f6e
daniele <daniele.fallaci@gmail.com>
2025-01-10 19:48:24 +0100

    bootloader upd

--- hosts/danixOS/bootloader.nix
+++ hosts/danixOS/bootloader.nix
@@ -2,26 +2,27 @@
 {inputs, pkgs, ...}: 
 
 {
-  #boot.loader.systemd-boot.enable = true;
-  #boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
 
-  boot.loader = {
-        grub = {
-            enable                = true;
-            useOSProber           = true;
-            copyKernels           = true;
-            efiInstallAsRemovable = true;
-            efiSupport            = true;
-            fsIdentifier          = "label";
-            devices               = [ "nodev" ];
-            extraEntries = ''
-                menuentry "Reboot" {
-                    reboot
-                }
-                menuentry "Poweroff" {
-                    halt
-                }
-            '';
-        };
-    };
+#   boot.loader = {
+#         grub = {
+#             enable                = true;
+#             useOSProber           = true;
+#             copyKernels           = true;
+#             efiInstallAsRemovable = true;
+#             efiSupport            = true;
+#             fsIdentifier          = "label";
+#             devices               = [ "/boot" ];
+#             extraEntries = ''
+#                 menuentry "Reboot" {
+#                     reboot
+#                 }
+#                 menuentry "Poweroff" {
+#                     halt
+#                 }
+#             '';
+#         };
+#     };
 }
