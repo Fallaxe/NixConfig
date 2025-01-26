ed22aac79cf29b612e7c740058abff7181a50463
daniele <daniele.fallaci@gmail.com>
2025-01-26 13:54:04 +0100

    added eww new widg

--- hosts/danixOS/home/home-modules/eww/default.nix
+++ hosts/danixOS/home/home-modules/eww/default.nix
@@ -2,10 +2,10 @@
 { config, pkgs, inputs, ... }:
 {
   home.packages = [
-    pkgs.eww
+    pkgs.eww-wayland
   ];
   
-  home.file.".config/eww/".source=./hellow;
+  home.file.".config/eww/".source=./eww-widgets/eww;
   # programs.eww = {
   #   enable = true;
   #   configDir = "${config.home.homeDirectory}/.config/eww";
