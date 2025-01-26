8777ba0e72b19da0e695b9b10c29a5de3e95fdb2
daniele <daniele.fallaci@gmail.com>
2025-01-26 13:56:51 +0100

    a

--- hosts/danixOS/home/home-modules/eww/default.nix
+++ hosts/danixOS/home/home-modules/eww/default.nix
@@ -5,7 +5,7 @@
     pkgs.eww-wayland
   ];
   
-  home.file.".config/eww/".source=./eww-widgets/eww;
+  home.file.".config/eww/".source=./eww-widgets/eww/bar;
   # programs.eww = {
   #   enable = true;
   #   configDir = "${config.home.homeDirectory}/.config/eww";
